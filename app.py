14884298aeaa07a30ab497cb3dda15daf9d9df29
jurody <kamilov.2000@mail.ru>
2021-11-24 16:42:33 +0500

    errorhandler off

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from routes import *
 from werkzeug.exceptions import HTTPException
 
-@app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('/pages/error/404.html'), 404
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     # note that we set the 404 status explicitly
+#     return render_template('/pages/error/404.html'), 404
 
-@app.errorhandler(500)
-def server_error(e):
-    print("500 called")
-    # note that we set the 404 status explicitly
-    return render_template('/pages/error/500.html'), 500
+# @app.errorhandler(500)
+# def server_error(e):
+#     print("500 called")
+#     # note that we set the 404 status explicitly
+#     return render_template('/pages/error/500.html'), 500
 
-@app.errorhandler(HTTPException)
-def handle_exception(e):
-    print("HTTPException called")
-    return render_template('/pages/error/500.html'), e.code
+# @app.errorhandler(HTTPException)
+# def handle_exception(e):
+#     print("HTTPException called")
+#     return render_template('/pages/error/500.html'), e.code
 
 
 # @app.errorhandler(Exception)
