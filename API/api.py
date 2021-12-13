fa63086e9825fbd6854581b266cc383fce90dd00
Odilxon <mmuhtor@gmail.com>
2021-12-13 10:29:44 +0500

    fix

--- API/api.py
+++ API/api.py
@@ -190,8 +190,7 @@ def user_tasks(c):
         T['attachments'] = [x.format() for x in Attachment.query.filter(Attachment.type=='task', Attachment.type_id==task.id).all()]
         T['history'] = [ x.format() for x in Task_History.query.filter(Task_History.task_id == task.id).all()]
         T['isAdmin'] = True if db.session.query(User).filter(User.id == task.owner_id).first().role == 'admin' else False
-        if foradmin:
-            T['users'] = fetchUsers(T)
+        T['users'] = fetchUsers(T)
         ret_data.append(T)
     return jsonify(ret_data)
 
